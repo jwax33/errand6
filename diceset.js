72d4f4b156e8ef0dcbfad1b8dece85bfc4a8dd05
jwax33 <drose25@gmail.com>
2024-03-16 20:59:22 -0500

    Create diceset.js

---
+++ diceset.js
@@ -0,0 +1,9 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({id: "errand6", name: "errand6"}, false);
+	dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/errand6/models/errand6.gltf",
+		system: "errand6"
+	});
+});
